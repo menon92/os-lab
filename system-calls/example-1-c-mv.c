8e53d171063f76e036d1877a64911fb4825a4b53
Mehadi Hasan Menon <mehadihasan80@gmail.com>
2017-11-08 13:09:18 +0600

    Update example-1-c-mv.c

--- system-calls/example-1-c-mv.c
+++ system-calls/example-1-c-mv.c
@@ -1,7 +1,7 @@
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<stdio.h>
-#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
 
 int main(int argc,char *argv[] )
 {
